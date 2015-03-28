42a33fb7b8fb5b190a61966a402f8cc3b9f49800
Noah Kobayashi <vohedge@gmail.com>
2015-03-28 14:14:23 +0900

    Add tests

--- test/unit/email_filter_test.rb
+++ test/unit/email_filter_test.rb
@@ -5,16 +5,24 @@ class EmailFilterTest < ActiveSupport::TestCase
 
   fixtures :projects
 
+  FIXTURES_PATH = File.dirname(__FILE__) + '/../fixtures/email'
+
   def setup
     @project = Project.find(1)
     # @email_filter = EmailFilter.new
   end
 
+  ################################################################################
+  #
+  # Validation Tests
+  #
+  ################################################################################
+
   # PRESENCE
 
   def test_email_filter_test_data_valid
     email_filter = create_email_filter
-    assert email_filter.save
+    assert_equal true, email_filter.save
   end
 
   def test_email_filter_name_presence
@@ -76,7 +84,7 @@ class EmailFilterTest < ActiveSupport::TestCase
     %w{1 100}.each do |id|
       email_filter = create_email_filter
       email_filter.project_id = id 
-      assert email_filter.save
+      assert_equal true, email_filter.save
     end
   end
 
@@ -89,7 +97,7 @@ class EmailFilterTest < ActiveSupport::TestCase
     %w{1 12 156}.each do |position|
       email_filter = create_email_filter
       email_filter.position = position 
-      assert email_filter.save
+      assert_equal true, email_filter.save
     end
   end
 
@@ -104,7 +112,7 @@ class EmailFilterTest < ActiveSupport::TestCase
     %w{and or}.each do |operator|
       email_filter = create_email_filter
       email_filter.operator = operator
-      assert email_filter.save
+      assert_equal true, email_filter.save
     end
   end
 
@@ -117,7 +125,7 @@ class EmailFilterTest < ActiveSupport::TestCase
     %w{from to cc subject body}.each do |email_field|
       email_filter = create_email_filter
       email_filter.email_filter_conditions.first.email_field = email_field
-      assert email_filter.save
+      assert_equal true, email_filter.save
     end
   end
 
@@ -130,8 +138,222 @@ class EmailFilterTest < ActiveSupport::TestCase
     %w{include not_include}.each do |match_type|
       email_filter = create_email_filter
       email_filter.email_filter_conditions.first.match_type = match_type
-      assert email_filter.save
+      assert_equal true, email_filter.save
     end
+  end
+
+  ################################################################################
+  #
+  # Single Condition Tests
+  #
+  ################################################################################
+
+  def test_email_filter_to
+    email = load_email('email001.eml')
+    email_filter = create_email_filter
+
+    condition = EmailFilterCondition.new(email_field: 'to',
+                                         match_type: 'include',
+                                         match_text: 'redmine')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'to',
+                                         match_type: 'include',
+                                         match_text: 'ruby')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'to',
+                                         match_type: 'not_include',
+                                         match_text: 'ruby')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'to',
+                                         match_type: 'not_include',
+                                         match_text: 'redmine')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+  end
+
+  def test_email_filter_from
+    email = load_email('email001.eml')
+    email_filter = create_email_filter
+
+    condition = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'include',
+                                         match_text: 'jsmith')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'include',
+                                         match_text: 'taro')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'not_include',
+                                         match_text: 'taro')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'not_include',
+                                         match_text: 'jsmith')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+  end
+
+  def test_email_filter_cc
+    email = load_email('email001.eml')
+    email_filter = create_email_filter
+
+    condition = EmailFilterCondition.new(email_field: 'cc',
+                                         match_type: 'include',
+                                         match_text: 'bob@somenet.foo')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'cc',
+                                         match_type: 'include',
+                                         match_text: 'taro')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'cc',
+                                         match_type: 'not_include',
+                                         match_text: 'taro')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'cc',
+                                         match_type: 'not_include',
+                                         match_text: 'bob@somenet.foo')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+  end
+
+  def test_email_filter_subject
+    email = load_email('email001.eml')
+    email_filter = create_email_filter
+
+    condition = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'include',
+                                         match_text: 'testing!')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'include',
+                                         match_text: 'Hello!')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'not_include',
+                                         match_text: 'Hello!')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'not_include',
+                                         match_text: 'testing!')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+  end
+
+  def test_email_filter_body
+    email = load_email('email001.eml')
+    email_filter = create_email_filter
+
+    condition = EmailFilterCondition.new(email_field: 'body',
+                                         match_type: 'include',
+                                         match_text: 'Lorem ipsum')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'body',
+                                         match_type: 'include',
+                                         match_text: 'Ruby on rails')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'body',
+                                         match_type: 'not_include',
+                                         match_text: 'Ruby on rails')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition = EmailFilterCondition.new(email_field: 'body',
+                                         match_type: 'not_include',
+                                         match_text: 'Lorem ipsum')
+    email_filter.email_filter_conditions = [condition]
+    assert_equal false, email_filter.applicable?(email)
+  end
+
+  ################################################################################
+  #
+  # Multi Conditions Tests
+  #
+  ################################################################################
+
+  def test_email_filter_operator_and
+    email = load_email('email001.eml')
+    email_filter = create_email_filter
+
+    email_filter.operator = 'and'
+    condition1 = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'include',
+                                         match_text: 'jsmith@somenet.foo')
+    condition2 = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'include',
+                                         match_text: 'testing!')
+    email_filter.email_filter_conditions = [condition1,condition2]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition1 = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'include',
+                                         match_text: 'jsmith@somenet.foo')
+    condition2 = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'include',
+                                         match_text: 'Hello!')
+    email_filter.email_filter_conditions = [condition1,condition2]
+    assert_equal false, email_filter.applicable?(email)
+  end
+
+  def test_email_filter_operator_or
+    email = load_email('email001.eml')
+    email_filter = create_email_filter
+
+    email_filter.operator = 'or'
+    condition1 = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'include',
+                                         match_text: 'jsmith@somenet.foo')
+    condition2 = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'include',
+                                         match_text: 'testing!')
+    email_filter.email_filter_conditions = [condition1,condition2]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition1 = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'include',
+                                         match_text: 'jsmith@somenet.foo')
+    condition2 = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'include',
+                                         match_text: 'Hello!')
+    email_filter.email_filter_conditions = [condition1,condition2]
+    assert_equal true, email_filter.applicable?(email)
+
+    condition1 = EmailFilterCondition.new(email_field: 'from',
+                                         match_type: 'include',
+                                         match_text: 'noah@somenet.foo')
+    condition2 = EmailFilterCondition.new(email_field: 'subject',
+                                         match_type: 'include',
+                                         match_text: 'Hello!')
+    email_filter.email_filter_conditions = [condition1,condition2]
+    assert_equal false, email_filter.applicable?(email)
   end
 
   private
@@ -145,6 +367,12 @@ class EmailFilterTest < ActiveSupport::TestCase
                                                  match_type: 'include',
                                                  match_text: 'testing')
       email_filter
+    end
+
+    def load_email(filename, options={})
+      raw = IO.read(File.join(FIXTURES_PATH, filename))
+      yield raw if block_given?
+      Mail.new(raw)
     end
 
 
